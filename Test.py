bb1bcd28fd33761eac1e9213acbcf4a20a6c394b
timparaev <tim@paraev.com>
2025-09-11 08:51:04 +0100

    Homework 11 Sep 2025 - 70th factor

--- Test.py
+++ Test.py
@@ -1,1 +1,25 @@
-test2
+import math
+num = 1000000000
+factors = []
+for i in range(1, round(num/2)):
+    if num % i == 0:
+        factors.append(i)
+    if len(factors)<71:
+        continue
+    else:
+        break
+
+print(factors[69])
+
+num = 1000000000000
+factors = []
+for i in range(1, round(num/2)):
+    if num % i == 0:
+        factors.append(i)
+    if len(factors)<71:
+        continue
+    else:
+        break
+
+print(factors[69])
+
